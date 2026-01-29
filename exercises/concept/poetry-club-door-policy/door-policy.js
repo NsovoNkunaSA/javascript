42702e3f3d80fbf4a82b2b177188da1ca2abe4ba
Nsovo Nkuna <112828572+NsovoNkunaSA@users.noreply.github.com>
2026-01-29 18:02:05 +0200

    Update door-policy.js

--- exercises/concept/poetry-club-door-policy/door-policy.js
+++ exercises/concept/poetry-club-door-policy/door-policy.js
@@ -27,7 +27,7 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  return line.trimStart()[0];
 }
 
 /**
@@ -38,7 +38,8 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  if (!word) return '';
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
 
 /**
@@ -49,7 +50,8 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  const trimmed = line.trim();
+  return trimmed[trimmed.length - 1];
 }
 
 /**
@@ -60,5 +62,6 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  const formatted = word[0].toUpperCase() + word.slice(1).toLowerCase();
+  return `${formatted}, please`;
 }
